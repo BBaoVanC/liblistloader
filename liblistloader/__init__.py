5ae1a62c6cf18e7bc52d64c24d73a3913624150b
BBaoVanC <bbaovanc@protonmail.com>
2020-03-18 18:06:50 -0500

    v1.0

---
+++ liblistloader/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Word List Loader.
+
+by BBaoVanC
+
+This package allows for easy loading of word lists.
+Built for https://github.com/BBaoVanC/libnamegen/
+
+Copyright (C) 2020 BBaoVanC
+"""
+
+# Imports
+import liblistloader.desiquintans_nounlist  # NOQA
